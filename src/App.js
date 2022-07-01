5c79853d3eabeae3d39f7984e9a9da1ccccfa01c
Jimi Kapadia <jimikapadia24@gmail.com>
2022-07-01 21:37:39 +0530

    adding header

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header/>
       <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/shop" element={<ShopPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
       </Routes>
     </div>
   );
